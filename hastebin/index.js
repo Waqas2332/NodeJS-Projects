9a2253defb6de23ba0670ee6e4fdff8ec04e8da6
Waqas <wmunir232@gmail.com>
2023-11-09 16:02:53 +0500

    Made Hastebin Clone

---
+++ hastebin/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Document = require("./models/document");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  const code = `Welcome to CodeBin
+
+Use the commands in the top right corner
+to create a new file to share with others`;
+  res.render("code-display", { code, language: "plaintext" });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (error) {}
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value, id });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("new", { value: document.value });
+  } catch (error) {
+    res.redirect(`/${id}`);
+  }
+});
+
+app.listen(3000, function () {
+  mongoose
+    .connect(
+      "mongodb+srv://wmunir232:wmunir232@task-manager.sf6auvh.mongodb.net/"
+    )
+    .then(() => {
+      console.log("Connected to db");
+    })
+    .then(() => {
+      console.log("Server is running at port 3000");
+    });
+});
